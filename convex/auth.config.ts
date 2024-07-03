9de29bf2f25428e7cd4473004a3df6a9966f8679
RaizelShah <shahraizel11@gmail.com>
2024-07-03 23:30:07 +0530

    update backend

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://touched-peacock-9.clerk.accounts.dev
+
+export default {
+  providers: [
+    {
+      domain: "https://touched-peacock-9.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
